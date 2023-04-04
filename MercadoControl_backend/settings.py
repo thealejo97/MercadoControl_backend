d0ea02b6962d03ce8d7e23b6685ac6900bb57d46
Alejandro Montano <alejandromontano@useit.co>
2023-04-04 11:49:15 -0500

    Dockeriztion of the site

--- MercadoControl_backend/settings.py
+++ MercadoControl_backend/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_secret("DEBUG")
 
-ALLOWED_HOSTS = ['https://mercadocontrolback.fly.dev/','mercadocontrolback.fly.dev']
+ALLOWED_HOSTS = ['localhost','mercadocontrolback.fly.dev']
 
 
 # Application definition
