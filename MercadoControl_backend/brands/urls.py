dfdff330c97d12537a60035849ca520c956b0766
Alejandro Montano <alejandromontano@useit.co>
2023-04-05 12:57:49 -0500

    Fix error urls brands

--- MercadoControl_backend/brands/urls.py
+++ MercadoControl_backend/brands/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', CategoryListAPIView.as_view(), name='list_category'),
-    path('<int:pk>/', CategoryDetailAPIView.as_view(), name='detail_category'),
-    path('create/', CategoryCreateAPIView.as_view(), name='create_category'),
-    path('update/<int:pk>/', CategoryUpdateAPIView.as_view(), name='update_category'),
-    path('delete/<int:pk>/', CategoryDeleteAPIView.as_view(), name='delete_category'),
+    path('', BrandListAPIView.as_view(), name='list_brand'),
+    path('<int:pk>/', BrandDetailAPIView.as_view(), name='detail_brand'),
+    path('create/', BrandCreateAPIView.as_view(), name='create_brand'),
+    path('update/<int:pk>/', BrandUpdateAPIView.as_view(), name='update_brand'),
+    path('delete/<int:pk>/', BrandDeleteAPIView.as_view(), name='delete_brand'),
 ]
